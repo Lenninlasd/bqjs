407563460312f90ebe2c923fa2ed54a8cb27060b
Armando Guzman <armandoc.guzman@gmail.com>
2018-10-24 15:26:35 -0500

    Icon de redes sociales. add redes. (#8)

--- header.js
+++ header.js
@@ -1,1 +1,24 @@
-export const header = `<h1>BarranquillaJS Meetup</h1>`;
+export const header = `<header id="headerBaqJS" class="headerBaqJS">
+  <style>
+      .headerBaqJS{
+          width: 100%;
+          display: flex;
+          justify-content: flex-end;
+      }
+      .icon-redes{
+          display: flex;
+      }
+      .icon-redes a {
+          color: white;
+          padding: 7px 8px;
+          margin-left: 12px;
+          border-radius: 50%;
+          border: 2px solid white;
+      }
+  </style>
+  <div class="icon-redes">
+      <a href="#"><i class="fab fa-instagram"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-meetup"></i></a>
+  </div>
+</header>`;
