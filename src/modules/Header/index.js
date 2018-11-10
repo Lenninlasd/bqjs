8f496d89aaf16951b1dc5ea9cc693443897d0d5e
Jesús Bossa <me@jesusbossa.com>
2018-11-09 21:50:10 -0500

    [Add] Navbar bottom added: social media was breaking the vieweport

--- src/modules/Header/index.js
+++ src/modules/Header/index.js
@@ -1,9 +1,9 @@
-import './styles.css';
+import styles from './styles.css';
 
 export const Header = 
 `
     <header>
-        <nav>
+        <nav class="${styles.top}">
             <div>
                 <img src="./src/img/BAQJS-02.png" alt="BarranquillaJS logo">
                 <h1>
@@ -22,6 +22,20 @@ export const Header =
             <div>
                 <a target="_blank" href="https://barranquillajs.slack.com/join/shared_invite/enQtNDI1OTYwOTE2MjQwLTJhYWIzOGJhZDQ3NDljYmMyZjNiMzUwYWM0ZGMwYTliMWRhYmQ2ZjVhODM4MjE2OTg4YTEwYTQzMjAzMzA1Mzc"><i class="fab fa-slack"></i></a>
             </div>
+        </nav>
+        <nav class="${styles.bottom}">
+        <div>
+            <a target="_blank" href="https://www.instagram.com/barranquillajs/"><i class="fab fa-instagram"></i></a>
+        </div>
+        <div>
+            <a target="_blank" href="https://twitter.com/baq_js"><i class="fab fa-twitter"></i></a>
+        </div>
+        <div>
+            <a target="_blank" href="https://github.com/barranquillajs"><i class="fab fa-github"></i></a>
+        </div>
+        <div>
+            <a target="_blank" href="https://barranquillajs.slack.com/join/shared_invite/enQtNDI1OTYwOTE2MjQwLTJhYWIzOGJhZDQ3NDljYmMyZjNiMzUwYWM0ZGMwYTliMWRhYmQ2ZjVhODM4MjE2OTg4YTEwYTQzMjAzMzA1Mzc"><i class="fab fa-slack"></i></a>
+        </div>
         </nav>
     </header>
 `
