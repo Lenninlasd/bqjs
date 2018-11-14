af1fc151c1f0a0dc54ae0629b5bbe9a839f0f052
Jesús David Bossa <me@jesusbossa.com>
2018-11-14 13:23:37 -0500

    Navbar responsive (#22)
    * [Improvement] Nabvar improved: display grid dinamic
    
    * [Add] Navbar bottom added: social media was breaking the vieweport
    
    * [Improvement] Logo size improvement

--- src/modules/Header/index.js
+++ src/modules/Header/index.js
@@ -1,21 +1,41 @@
 import styles from './styles.css';
 
-export const Header = `
-    <div class="${styles.baqContainer}">
-        <header class="${styles.navbarBaq}">
-
-            <div class="${styles.navbarLeftText}">
+export const Header = 
+`
+    <header>
+        <nav class="${styles.top}">
+            <div>
                 <img src="./src/img/BAQJS-02.png" alt="BarranquillaJS logo">
-                <h1 class="${styles.title}">
+                <h1>
                     BarranquillaJS
                 </h1>
             </div>
-
-            <div class="${styles.iconRedes}">
+            <div>
                 <a target="_blank" href="https://www.instagram.com/barranquillajs/"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div>
                 <a target="_blank" href="https://twitter.com/baq_js"><i class="fab fa-twitter"></i></a>
+            </div>
+            <div>
                 <a target="_blank" href="https://github.com/barranquillajs"><i class="fab fa-github"></i></a>
+            </div>
+            <div>
                 <a target="_blank" href="https://barranquillajs.slack.com/join/shared_invite/enQtNDI1OTYwOTE2MjQwLTJhYWIzOGJhZDQ3NDljYmMyZjNiMzUwYWM0ZGMwYTliMWRhYmQ2ZjVhODM4MjE2OTg4YTEwYTQzMjAzMzA1Mzc"><i class="fab fa-slack"></i></a>
             </div>
-        </header>
-    </div>`
+        </nav>
+        <nav class="${styles.bottom}">
+        <div>
+            <a target="_blank" href="https://www.instagram.com/barranquillajs/"><i class="fab fa-instagram"></i></a>
+        </div>
+        <div>
+            <a target="_blank" href="https://twitter.com/baq_js"><i class="fab fa-twitter"></i></a>
+        </div>
+        <div>
+            <a target="_blank" href="https://github.com/barranquillajs"><i class="fab fa-github"></i></a>
+        </div>
+        <div>
+            <a target="_blank" href="https://barranquillajs.slack.com/join/shared_invite/enQtNDI1OTYwOTE2MjQwLTJhYWIzOGJhZDQ3NDljYmMyZjNiMzUwYWM0ZGMwYTliMWRhYmQ2ZjVhODM4MjE2OTg4YTEwYTQzMjAzMzA1Mzc"><i class="fab fa-slack"></i></a>
+        </div>
+        </nav>
+    </header>
+`
